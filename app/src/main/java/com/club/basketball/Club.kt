4804061760a55c3f042b2625cd35658aab791c6c
Afianfebrianto <afianfebrianto88@gmail.com>
2023-11-26 21:46:45 +0800

    Penambahan Data class club2 Club.kt

--- app/src/main/java/com/club/basketball/Club.kt
+++ app/src/main/java/com/club/basketball/Club.kt
@@ -11,3 +11,12 @@ data class Club(
     val location: String,
     val photo: Int
 ) : Parcelable
+
+@Parcelize
+data class Club2(
+    val name: String,
+    val description: String,
+    val faounded: String,
+    val location: String,
+    val photo: Int
+) : Parcelable
